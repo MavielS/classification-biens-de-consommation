bc43aa50cafdfb0826f4f9443cd99fb397975f63
MavielS <sebastien.maviel@gmail.com>
2021-08-30 18:57:42 +0200

    add random forests classification

--- p6_utils.py
+++ p6_utils.py
@@ -14,6 +14,7 @@ import exploration as ex
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import adjusted_rand_score, silhouette_score, davies_bouldin_score, calinski_harabasz_score
+from sklearn.preprocessing import StandardScaler
 
 
 plt.style.use('fivethirtyeight')
@@ -78,7 +79,7 @@ def vectorized_ngrams(data, vec, min_words:list, ngram:tuple, debug=True):
             # Materialize the sparse data
             data_dense = vectorized_words_values[ind].todense()
 
-            print(min_words[ind], end=' mots -- ')
+            print(min_words[ind], end=' occurences -- ')
              # Compute Sparsicity = Percentage of Non-Zero cells
             print(f"(ngramm:{ngram}) Taille: {vectorized_words_values[ind].shape}, Sparcity: {((data_dense > 0).sum()/data_dense.size)*100:.3f} %")
 
@@ -92,7 +93,12 @@ def plot_aris_score(list_vectorized_values, model, min_words, true_labels, title
 
     for ind in range(0,len(list_vectorized_values)):
         X = list_vectorized_values[ind]
-        model.fit(X)
+        
+        # Centrage et Réduction
+        std_scale = StandardScaler().fit(X)
+        X_scaled = std_scale.transform(X)
+        
+        model.fit(X_scaled)
 
         aris.append(adjusted_rand_score(true_labels, model.labels_))
 
@@ -121,7 +127,15 @@ def plot_all_aris_score(vectorized_values_list: list, true_labels, x_value, mode
         # Calcule l'ARI pour une configuration donnée
         for ind in range(0,len(data)):
             X = data[ind]
-            model.fit(X)
+            if type(X) != np.ndarray:
+                X = X.toarray()
+            
+            # Centrage et Réduction
+            std_scale = StandardScaler().fit(X)
+            X_scaled = std_scale.transform(X)
+            
+            model.fit(X_scaled)
+            
             aris.append(adjusted_rand_score(true_labels, model.labels_))
             
         # Affiche ce score sur notre figure
@@ -131,3 +145,43 @@ def plot_all_aris_score(vectorized_values_list: list, true_labels, x_value, mode
     fig.update_layout(title=title, width=2000, height=800, xaxis_title="Occurence mini.", yaxis_title="Score ARI")
     
     fig.show()
+    
+    
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+
+def reduce_variables_with_pca(list_vectorized_values, th, debug=True):
+    to_return = []
+    
+    for ind,_ in enumerate(list_vectorized_values):
+        X = list_vectorized_values[ind].toarray()
+    #     products = list(range(0,1050,1)) 
+    #     features = vectorizers.get_feature_names() # Je stocke le nom des colonnes (mes mots)
+        n_comp = X.shape[1] if X.shape[1]<X.shape[0] else X.shape[0] # On calcule le max de composantes principales
+        
+
+        # Centrage et Réduction
+        std_scale = StandardScaler().fit(X)
+        X_scaled = std_scale.transform(X)
+
+        # Calcul des composantes principales
+        pca = PCA(n_components=n_comp, random_state=0)
+        pca.fit(X_scaled)    
+
+        # Je détermine le nombre de composantes principales pour avoir th % de ma variance expliquée
+        tot_variance_explained = 0
+        nb_components = 0
+        while(tot_variance_explained < th and nb_components < n_comp):
+            tot_variance_explained += pca.explained_variance_ratio_[nb_components]
+            nb_components +=1
+            
+        if debug:
+            print(f'{tot_variance_explained*100}% de variance expliquée avec {nb_components} composants principaux.')
+
+        # Recalcul des composantes principales
+        pca = PCA(n_components=nb_components, random_state=0)
+        pca.fit(X_scaled)  
+
+        to_return.append(pca.transform(X))
+    return to_return
